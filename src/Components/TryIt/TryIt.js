682eeeaa677bd1fff32727d30d3770b07e392eb9
Courtney Olesee <court.olson1@gmail.com>
2020-01-14 08:53:29 -0600

    try it page started: btns and routes on, progress bars & add btn in

--- src/Components/TryIt/TryIt.js
+++ src/Components/TryIt/TryIt.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import WaterProgressBar from '../WaterProgressBar/WaterProgressBar';
+import WasteProgressBar from '../WasteProgressBar/WasteProgressBar';
+import ItemCard from '../ItemCard/ItemCard';
+
+// material UI
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import classNames from 'classnames';
+
+const styles = theme => ({
+    fab: {
+      margin: theme.spacing(2),
+    },
+    button: {
+        margin: theme.spacing(),
+      },
+      input: {
+        display: 'none',
+    },
+    headerbtns: {
+        color: 'white',
+        backgroundColor: '#d6a05e',
+    },
+    cssRoot: {
+        color: 'white',
+        backgroundColor: '#F28705',
+    },
+  });
 
 class TryIt extends Component {
 
+    // ROUTES
     goToAbout = ()=>{
         this.props.history.push(`/about`);
     }
@@ -15,18 +46,56 @@ class TryIt extends Component {
         this.props.history.push(`/signup`);
     }
 
+    goToGraph = () => {
+        this.props.history.push(`/graph`);
+    }
+
+    goToAccountSettings = () => {
+        this.props.history.push(`/account`)
+    }
+
+    goToAddNew = () => {
+        this.props.history.push(`/add`)
+    }
+
+    // RENDER
     render(){
+        const { classes } = this.props;
         return (
-        <div>
-            <h2>Try It!</h2>
-            <section>
-                <button onClick={this.goToAbout}>About</button>
-                <button onClick={this.goToLogin}>Login</button>
-                <button onClick={this.goToSignUp}>Sign Up</button>
-            </section>
-        </div> 
+            <div>
+                <section>
+                    <h1>
+                    <Button size="small" variant="contained" color="primary" className={classNames(classes.margin, classes.headerbtns)}
+                    onClick={this.goToAbout}>About</Button>
+                    <Button size="small" variant="contained" color="primary" className={classNames(classes.margin, classes.headerbtns)}
+                    onClick={this.goToLogin}>Login</Button>
+                    <Button size="small" variant="contained" color="primary" className={classNames(classes.margin, classes.headerbtns)}
+                    onClick={this.goToSignUp}>Sign Up</Button><br/>
+                    Try It!
+                    </h1>
+                </section>
+                <WaterProgressBar />
+                <WasteProgressBar />
+                <section className="closetSticky">
+                    <Button size="small" variant="contained" color="primary" className={classNames(classes.margin, classes.cssRoot)} onClick={this.goToGraph}>
+                        Graph
+                    </Button>
+                    <Button size="small" variant="contained" color="primary" className={classNames(classes.margin, classes.cssRoot)} onClick={this.goToAccountSettings}>
+                    Account Settings
+                    </Button>
+                </section>
+                <section className="closet">
+                    Closet 
+                    <Fab color="primary" aria-label="Add" onClick={this.goToAddNew} style={{backgroundColor:"#03A696", marginLeft:"300px"}} size="small">
+                        <AddIcon />
+                    </Fab>
+                    <ItemCard />
+                </section>
+            </div>
         )
     }
 }
 
-export default connect() (TryIt);
+export default withStyles(styles)(TryIt);
+
+
